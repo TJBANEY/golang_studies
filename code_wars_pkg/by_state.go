c171797526aba7c0644d87df77cbd02b74ede4dc
Timothy Baney <baney-surv@hotmail.com>
2018-12-28 22:39:40 -0600

    newPkg: CodeWar#3 - byState

---
+++ code_wars_pkg/by_state.go
@@ -0,0 +1,22 @@
+package codeWars
+
+// Given a string with friends to visit in different states:
+
+// ad3="John Daggett, 341 King Road, Plymouth MA
+// Alice Ford, 22 East Broadway, Richmond VA
+// Sal Carpenter, 73 6th Street, Boston MA"
+
+// we want to produce a result that sorts the names by state and lists the name of the state
+// followed by the name of each person residing in that state (people's names sorted). When the result is printed we get:
+
+func byState(addressList string) (stateString string) {
+
+	return "some string"
+}
+
+// Example result
+// Massachusetts
+// ..... John Daggett 341 King Road Plymouth Massachusetts
+// ..... Sal Carpenter 73 6th Street Boston Massachusetts
+// Virginia
+// ..... Alice Ford 22 East Broadway Richmond Virginia
--- code_wars_pkg/code_wars.go
+++ code_wars_pkg/code_wars.go
@@ -42,7 +42,7 @@ func CartesianNeighborAlternate(x, y int) [][]int {
 	return neighbors
 }
 
-// OtherCartesianSolution
+// OtherCartesianSolution - ..
 func OtherCartesianSolution(x, y int) [][]int {
 	return [][]int{{x - 1, y - 1}, {x, y - 1}, {x + 1, y - 1}, {x - 1, y}, {x + 1, y}, {x - 1, y + 1}, {x, y + 1}, {x + 1, y + 1}}
 }
